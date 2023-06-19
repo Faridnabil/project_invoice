2e9c917d4bdf69178fb700790f1836e99345e92a
Muhammad Dwiki Yudhistira <yudhistiradwiki17@gmail.com>
2023-06-19 11:03:50 +0700

    Kop surat delete header footer
    css edit to delete header and footer information.

--- resources/views/berita-acara/beritaprint.blade.php
+++ resources/views/berita-acara/beritaprint.blade.php
@@ -1,77 +1,93 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Berita Acara</title>
-    <style type="text/css">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Cetak Laporan Data Alumni</title>
+    <style>
         body {
-
-            background-color: #fff;
+            font-family: "Times New Roman", Times, serif;
+        }
+        @page {
+            margin-top: 0;
+            margin-bottom: 0;
         }
 
-        .atas {
-            border-bottom: 3px solid #000;
-            padding: 0px;
-        }
-
-        .tengah {
-            line-height: 3px;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        p {
-            font-family: 'Times New Roman', Times, serif;
-            color: #000;
-        }
-
-        th {
-            text-align: left;
+        header,
+        footer {
+            display: none;
         }
     </style>
 </head>
 
 <body>
-    <div class="rangkasurat">
-        {{-- <table class="atas" width="100%">
+    <center>
+        <table width="670px" cellspacing="0" cellpadding="0">
             <tr>
-                <td><img src="theme/images/logo-mini.png" width="130px"></td>
-                <td class="tengah">
-                    YAYASAN PENDIDIKAN INDORAMA<br>
-                    <h2>POLITEKNIK ENJINERING INDORAMA</h2>
-                    <h4>Kembangkuning, Jatiluhur</h4>
-                    <h3>PURWAKARTA - 41152</h3>
-                    <font size="9px"><i>Telp. (0264) 8301041 - 43, Fax. (0264) 202318, Website:http://www.pei.ac.id,
-                            Email:info@pei.ac.id</i></font>
+                <td>
+                    <center><img src="{{ asset('/template/dist/img/logo-global.png') }} width="150px" /></center>
+                </td>
+                <td colspan="11">
+                    <h3
+                        style=" text-align: right; margin-top: 5px; font-size: 12px; font-family: Arial, Helvetica, sans-serif; font-weight: bolder;">
+                        PT GLOBAL TECHNOLOGY ESSENTIAL</h3>
+                    <p
+                        style=" text-align: right; margin-top: -25px; font-size: 11px; font-family: Arial, Helvetica, sans-serif;">
+                        <br />
+                        <i>Bumi Jaya Indah E 12 A, Purwakarta, Jawa Barat, 41117</i><br>
+                        globaltechnologyessential@gmail.com
+
+                    </p>
                 </td>
             </tr>
-        </table> --}}
-        <p align="center" style="font-size: 20px"><b><u>BERITA ACARA SERAH TERIMA</u></b></p>
-        <br>
-        <p>
-            Pada hari ini tanggal, bla bla telah dilakukan serah terima hasil pekerjaan oleh dan diantara:
-            <br>
-            1. Nama : GEN Z Company <br>
-            Perusahaan : SDASDa <br>
-            Alamat : asdasdasdasd <br><br>
-            Selanjutnya disebut sebagai <b>"PIHAK PERTAMA"</b>. <br><br>
+            <tr>
+                <td colspan="12">
+                    <hr />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="12"><br /></td>
+            </tr>
 
-            <br>
-            2. Nama : GEN Z Company <br>
-            Perusahaan : SDASDa <br>
-            Alamat : asdasdasdasd <br><br>
-            Selanjutnya disebut sebagai <b>"PIHAK KEDUA"</b>. <br><br>
+        </table>
+    </center>
+    </td>
+    </tr>
+    </center>
 
-            Dengan ini menerangkan bahwa : <br><br>
-            PIHAK KEDUA telah menyerahkan kepada PIHAK PERTAMA, dan PIHAK PERTAMA menyatakan telah menerima hasil pekerjaan untuk pekerjaan “Perbaikan 10 Unit Air Conditioner di Departemen Pendidikan dan Kebudayaan Yogyakarta” yang diselesaikan pada tanggal 26 Maret 2021.
-            Dengan adanya Berita Acara Serah Terima Pekerjaan ini maka PIHAK KEDUA telah melaksanakan kewajiban dengan baik dan berhak menerima pembayaran dari PIHAK PERTAMA sebesar Rp 2.500.000,- dibayar tunai sesuai perjanjian awal.
-            Demikian Berita Acara Serah Terima Kedua ini dibuat rangkap 3 (tiga) untuk dipergunakan sebagaimana mestinya.
-            <br>
+    Pada hari ini tanggal, bla bla telah dilakukan serah terima hasil pekerjaan oleh dan diantara:
+    <br>
+    1. Nama : GEN Z Company <br>
+    Perusahaan : SDASDa <br>
+    Alamat : asdasdasdasd <br><br>
+    Selanjutnya disebut sebagai <b>"PIHAK PERTAMA"</b>. <br><br>
 
-        </p>
-        <p style="text-align: right;">Purwakarta, 16 Juni 2023<br>Mengetahui,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <br><br><br><br><br>GEN Z Company
-        </p>
+    <br>
+    2. Nama : GEN Z Company <br>
+    Perusahaan : SDASDa <br>
+    Alamat : asdasdasdasd <br><br>
+    Selanjutnya disebut sebagai <b>"PIHAK KEDUA"</b>. <br><br>
+
+    Dengan ini menerangkan bahwa : <br><br>
+    PIHAK KEDUA telah menyerahkan kepada PIHAK PERTAMA, dan PIHAK PERTAMA menyatakan telah menerima hasil pekerjaan
+    untuk pekerjaan “Perbaikan 10 Unit Air Conditioner di Departemen Pendidikan dan Kebudayaan Yogyakarta” yang
+    diselesaikan pada tanggal 26 Maret 2021.
+    Dengan adanya Berita Acara Serah Terima Pekerjaan ini maka PIHAK KEDUA telah melaksanakan kewajiban dengan baik dan
+    berhak menerima pembayaran dari PIHAK PERTAMA sebesar Rp 2.500.000,- dibayar tunai sesuai perjanjian awal.
+    Demikian Berita Acara Serah Terima Kedua ini dibuat rangkap 3 (tiga) untuk dipergunakan sebagaimana mestinya.
+    <br>
+
+    </p>
+    <p style="text-align: right;">Purwakarta, 16 Juni 2023<br>Mengetahui,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br><br><br><br><br>GEN Z Company
+    </p>
     </div>
+    <script>
+        window.print();
+    </script>
 </body>
 
 </html>
+</body>
