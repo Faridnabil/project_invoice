0847f496cf8022ff8a960e5bfa1daa9f2821db88
Muhammad Dwiki Yudhistira <yudhistiradwiki17@gmail.com>
2023-07-04 14:07:14 +0700

    sisa amount due dan logout update

--- resources/views/menu/home.blade.php
+++ resources/views/menu/home.blade.php
@@ -97,71 +97,24 @@
                 </div>
                 <!-- /.row -->
 
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">CONTOH DATATABLE</h3>
+                <div class="row">
+                    <div class="col-lg-12 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>Rp. {{ number_format($amount_due, 0, ',', '.') }}</h3>
+
+                                <p>Amount Due</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-money-check-alt"></i>
+                            </div>
+                        </div>
                     </div>
-                    <!-- /.card-header -->
-                    <div class="card-body">
-                        <table id="example1" class="example1 table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Rendering engine</th>
-                                    <th>Browser</th>
-                                    <th>Platform(s)</th>
-                                    <th>Engine version</th>
-                                    <th>CSS grade</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Trident</td>
-                                    <td>Internet
-                                        Explorer 4.0
-                                    </td>
-                                    <td>Win 95+</td>
-                                    <td> 4</td>
-                                    <td>X</td>
-                                </tr>
-                                <tr>
-                                    <td>Trident</td>
-                                    <td>Internet
-                                        Explorer 5.0
-                                    </td>
-                                    <td>Win 95+</td>
-                                    <td>5</td>
-                                    <td>C</td>
-                                </tr>
-                                <tr>
-                                    <td>Trident</td>
-                                    <td>Internet
-                                        Explorer 5.5
-                                    </td>
-                                    <td>Win 95+</td>
-                                    <td>5.5</td>
-                                    <td>A</td>
-                                </tr>
-                                <tr>
-                                    <td>Trident</td>
-                                    <td>Internet
-                                        Explorer 6
-                                    </td>
-                                    <td>Win 98+</td>
-                                    <td>6</td>
-                                    <td>A</td>
-                                </tr>
-                                <tr>
-                                    <td>Trident</td>
-                                    <td>Internet Explorer 7</td>
-                                    <td>Win XP SP2+</td>
-                                    <td>7</td>
-                                    <td>A</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- /.card-body -->
+                    <!-- ./col -->
+
                 </div>
+                <!-- /.row -->
 
             </div><!-- /.container-fluid -->
         </section>
